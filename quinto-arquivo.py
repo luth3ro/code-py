35c5fc3f92ea5273a63b85d51800e277390515bf
luth3ro <gacs111111@outlook.com>
2023-09-26 02:38:21 -0300

    Quinto arquivo, décimo segundo exercício

---
+++ quinto-arquivo.py
@@ -0,0 +1,23 @@
+'''nono exercício'''
+nomes = ['Ana','Carlos','Daiane','Fernando','Maria']
+print(nomes) #Mostra todos os nomes dentro das chaves
+
+#Décimo exercício
+nomes = ['Ana','Carlos','Daiane','Fernando','Maria']
+print(len(nomes)) #Len parametriza a quantidade de itens dentro do array
+print(nomes[3]) #esse print puxa o terceiro item do array
+
+#Décimo primeiro exercício
+num1=52 
+num2=106
+
+print(num1 + num2)
+
+#Décimo segundo exercício 
+num1 = 52
+num2 = 106 
+
+print(num1 - num2) 
+#ou 
+subtracao = (num1 - num2)
+print(subtracao)
